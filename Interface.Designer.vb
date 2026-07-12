867ef3f96bf6cff0724523efb3516a35774648b6
Peter Swinkels <peter_swinkels@hotmail.com>
2026-07-12 18:43:19 +0200

    Add files via upload

--- Interface.Designer.vb
+++ Interface.Designer.vb
@@ -23,39 +23,56 @@ Partial Class InterfaceWindow
    <System.Diagnostics.DebuggerStepThrough()> _
    Private Sub InitializeComponent()
       Me.components = New System.ComponentModel.Container()
-      Me.ImageBox = New System.Windows.Forms.PictureBox()
-      Me.ToolTip = New System.Windows.Forms.ToolTip(Me.components)
-      CType(Me.ImageBox, System.ComponentModel.ISupportInitialize).BeginInit()
-      Me.SuspendLayout()
-      '
-      'ImageBox
-      '
-      Me.ImageBox.BackColor = System.Drawing.Color.White
-      Me.ImageBox.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
-      Me.ImageBox.Dock = System.Windows.Forms.DockStyle.Fill
-      Me.ImageBox.Location = New System.Drawing.Point(0, 0)
-      Me.ImageBox.Name = "ImageBox"
-      Me.ImageBox.Size = New System.Drawing.Size(282, 253)
-      Me.ImageBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize
-      Me.ImageBox.TabIndex = 0
-      Me.ImageBox.TabStop = False
-      Me.ToolTip.SetToolTip(Me.ImageBox, "Press F1 for help.")
-      '
-      'InterfaceWindow
-      '
-      Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
-      Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-      Me.AutoScroll = True
-      Me.ClientSize = New System.Drawing.Size(282, 253)
-      Me.Controls.Add(Me.ImageBox)
-      Me.KeyPreview = True
-      Me.Name = "InterfaceWindow"
-      Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
-      CType(Me.ImageBox, System.ComponentModel.ISupportInitialize).EndInit()
-      Me.ResumeLayout(False)
-      Me.PerformLayout()
+        Me.ToolTip = New System.Windows.Forms.ToolTip(Me.components)
+        Me.ImagePanel = New System.Windows.Forms.Panel()
+        Me.ImageBox = New System.Windows.Forms.PictureBox()
+        Me.ImagePanel.SuspendLayout()
+        CType(Me.ImageBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'ImagePanel
+        '
+        Me.ImagePanel.AutoScroll = True
+        Me.ImagePanel.Controls.Add(Me.ImageBox)
+        Me.ImagePanel.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.ImagePanel.Location = New System.Drawing.Point(0, 0)
+        Me.ImagePanel.Name = "ImagePanel"
+        Me.ImagePanel.Size = New System.Drawing.Size(420, 342)
+        Me.ImagePanel.TabIndex = 1
+        '
+        'ImageBox
+        '
+        Me.ImageBox.BackColor = System.Drawing.Color.White
+        Me.ImageBox.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.ImageBox.Location = New System.Drawing.Point(0, 2)
+        Me.ImageBox.Margin = New System.Windows.Forms.Padding(2)
+        Me.ImageBox.Name = "ImageBox"
+        Me.ImageBox.Size = New System.Drawing.Size(282, 253)
+        Me.ImageBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize
+        Me.ImageBox.TabIndex = 1
+        Me.ImageBox.TabStop = False
+        Me.ToolTip.SetToolTip(Me.ImageBox, "Press F1 for help.")
+        '
+        'InterfaceWindow
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.AutoScroll = True
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(420, 342)
+        Me.Controls.Add(Me.ImagePanel)
+        Me.DoubleBuffered = True
+        Me.KeyPreview = True
+        Me.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.Name = "InterfaceWindow"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.ImagePanel.ResumeLayout(False)
+        Me.ImagePanel.PerformLayout()
+        CType(Me.ImageBox, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
 
-   End Sub
-   Friend WithEvents ImageBox As System.Windows.Forms.PictureBox
-   Friend WithEvents ToolTip As System.Windows.Forms.ToolTip
+    End Sub
+    Friend WithEvents ToolTip As System.Windows.Forms.ToolTip
+    Friend WithEvents ImagePanel As System.Windows.Forms.Panel
+    Friend WithEvents ImageBox As System.Windows.Forms.PictureBox
 End Class
